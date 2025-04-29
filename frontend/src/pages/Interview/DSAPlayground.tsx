f08a1ed1e8c5131c6b3bd1dcdbffb96de13a52b1
shreyeahhs <gowdashreyas364@gmail.com>
2025-04-29 16:03:51 +0530

    minor

--- frontend/src/pages/Interview/DSAPlayground.tsx
+++ frontend/src/pages/Interview/DSAPlayground.tsx
@@ -52,7 +52,7 @@ const DSAPlayground = () => {
     };
     socket.onmessage = (e) => {
       const data = e.data;
-      console.log(data)
+      console.log(e)
       if (data.event == "execution_result") {
         if (data.status == "successful") {
           console.log("Total Test Cases Passed: ", data.passed_count);
