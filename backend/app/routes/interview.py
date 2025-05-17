e1640fbc2a115224bbec48a15bdb5fcf3f829698
Nishikant Singh <nishikant@edudiagno.com>
2025-05-17 19:06:56 +0530

    added screenshot urls in interview data

--- backend/app/routes/interview.py
+++ backend/app/routes/interview.py
@@ -148,6 +148,14 @@ async def get_interview_recruiter_view(
         interview["video_url"] = (
             f"{config.settings.URL}/api/uploads/interview_video/{int(id)}/video.m3u8"
         )
+
+    interview["screenshot_urls"] = []
+    if os.path.exists(f"uploads/screenshot/{int(id)}/"):
+        print("*" * 50)
+        for f in os.listdir(f"uploads/screenshot/{int(id)}"):
+            interview["screenshot_urls"].append(
+                f"{config.settings.URL}/api/uploads/screenshot/{int(id)}/{f}"
+            )
     return interview
 
 
